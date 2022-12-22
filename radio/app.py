89cf7ca879dd9f1af0e41ee7cc7f7a661526dea7
valentinaovalle <113638635+valentinaovalle@users.noreply.github.com>
2022-12-21 19:09:08 -0500

    Update app.py

--- radio/app.py
+++ radio/app.py
@@ -178,7 +178,7 @@ def main(opt):
     )
 #INSERTAR LOGO EN LA BARRA LATERAL    
     with st.sidebar.container():
-     image =Image.open('PREVEO.png') 
+     image =Image.open('PREVEO.PNG') 
      new_image = image.resize((185, 160))
      st.image(new_image, width=None, use_column_width=False)
      
